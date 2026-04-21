4b146b0f654b8be4b209686bcf1a028f12736322
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-04-21 12:47:56 +0530

    MandalApplication.java main file

---
+++ src/main/java/com/Mandal/MandalApplication.java
@@ -0,0 +1,14 @@
+package com.Mandal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MandalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandalApplication.class, args);
+	}
+
+}
